da722490dc4cd8240943ea2dfccbf3c03765be0b
skatenewyorkcity <p.garboza@gmail.com>
2016-12-03 17:04:39 -0500

    commit12032016

---
+++ arithCalc/calculator.js
@@ -0,0 +1,11 @@
+﻿function vl(value) {
+    document.getElementById("screen").value += value;
+}
+function reset(value) {
+    document.getElementById("screen").value = "";
+}
+function solve(value) {
+    var solution = 0;
+    solution = document.getElementById("screen").value;
+    document.getElementById("screen").value = eval(solution);
+}
